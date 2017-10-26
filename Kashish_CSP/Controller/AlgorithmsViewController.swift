a920a7327ebd60c0a67a958447b58d84602acd62
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-26 12:11:00 -0600

    Created 7 new files and inherited from ViewController.swift for all of them.
    The file names are based of of the 7 Big ideas. Also the inheritence from ViewController will not remain the same moving on. This is to just have it there at the start.
    NOTE: import Foundation is not needed in the file right now because what we are doing will be handled by UIKit.

---
+++ Kashish_CSP/Controller/AlgorithmsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsViewController.swift
+//  Kashish_CSP
+//
+//  Created by Singh, Kashish on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class AlgorithmsViewController : UIViewController
+{
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ Kashish_CSP/Controller/InternetViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InternetViewController.swift
+//  Kashish_CSP
+//
+//  Created by Singh, Kashish on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class InternetViewController : UIViewController
+{
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
