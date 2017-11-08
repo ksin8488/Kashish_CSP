81806b903898df16299dfe78a3af9a700941bd92
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-11-08 13:28:01 -0700

    Created 2 new methods called setupAlgorithm() & createParagraphStyle() .
    Also connected the UILabel from the screen and called it "algorithmText" as well as learning about using "// MARK:" and "// TODO:" .

--- Kashish_CSP/Controller/AlgorithmsViewController.swift
+++ Kashish_CSP/Controller/AlgorithmsViewController.swift
@@ -10,8 +10,48 @@ import UIKit
 
 public class AlgorithmsViewController : UIViewController
 {
-    public override func viewDidLoad()
+    // MARK: Data members
+    @IBOutlet weak var algorithmText: UILabel!
+    
+    private func setupAlgorithm() -> Void
     {
-        super.viewDidLoad()
+        var algorithmSteps : [String] = []
+        
+        // TODO: Define algorithm and all steps
+        let algorithm :String = "These are tnsructions to create a project in Java using Eclipse and Github \n"
+        let stepOne :String = "First, ...."
+        let stepTwo :String = "Second, ...."
+        let stepThree :String = ".... ..."
+        
+        // TODO: Finish adding all the steps to the algorithm
+        algorithmSteps = [stepOne, stepTwo, stepThree]
+        
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
+        
+        for step in algorithmSteps
+        {
+            let bullet :String = "$"
+            let formattedStep :String = "\n\(bullet) \(step)"
+            let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let paragraphStyle = createParagraphStyle()
+            
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : paragraphStyle], range: NSMakeRange(0, attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep)
+        }
+        
+        algorithmText.attributedText = fullAttributedString
+    }
+    
+    private func createParagraphStyle() -> NSParagraphStyle
+    {
+        let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+        paragraphStyle.defaultTabInterval = 15
+        paragraphStyle.firstLineHeadIndent = 20
+        paragraphStyle.headIndent =  35
+        
+        return paragraphStyle
     }
 }
