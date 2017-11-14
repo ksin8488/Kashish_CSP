62afee2b545b303864bc824f9aaf0a4d84166dff
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-11-14 11:53:14 -0700

    Remade the waterScarcity.gif into a png extension.
    This was done to allow UIImage to display due to the .gif extension causing some issues for some reason in displaying the image when the program was run.

--- Kashish_CSP/Controller/ImpactViewController.swift
+++ Kashish_CSP/Controller/ImpactViewController.swift
@@ -13,13 +13,14 @@ public class ImpactViewController : UIViewController
     @IBOutlet weak var appDataPlan: UILabel!
     @IBOutlet weak var reasonExplination: UILabel!
     @IBOutlet weak var personalConnection: UILabel!
-    @IBOutlet weak var waterScarcity: UIImageView!
+
     @IBOutlet weak var appLayout: UIImageView!
     
     public override func viewDidLoad()
     {
         appLayout.image = UIImage(named: "GlobalImpactAppLayout")
-        reasonExplination.text = "here is a test phrase"
+        reasonExplination.text = "It is importaint to make sure people who need clean water get it."
+        personalConnection.text = "I have family in India and I know I would want them to have clean water."
         super.viewDidLoad()
     }
 }
