e65325f69058d1119f82aa4bed2a950536a8ef88
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-11-16 11:40:34 -0700

    Added 5 different images with 3 different sizes each for the Abstraction Screen Prep Assignment.

--- Kashish_CSP/Controller/ImpactViewController.swift
+++ Kashish_CSP/Controller/ImpactViewController.swift
@@ -16,12 +16,12 @@ public class ImpactViewController : UIViewController
 
     @IBOutlet weak var appLayout: UIImageView!
     
-    private func addImages() -> Void
+    private func addImages() -> Void        //adds an image to the UI programmatically
     {
         appLayout.image = UIImage(named: "GlobalImpactAppLayout")
     }
     
-    private func addText() -> Void
+    private func addText() -> Void      //adds text(s) to the label(s) programmatically
     {
         reasonExplination.text = "It is importaint to make sure people who need clean water get it."
         personalConnection.text = "I have family in India and I know I would want them to have clean water."
