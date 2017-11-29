c8793345cf2e9119e478841020aa4bf972b37ad1
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-11-29 12:01:43 -0700

    Created a new class and the code for the split view function.

---
+++ Kashish_CSP/Controller/InternetDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InternetDetailViewController.swift
+//  Kashish_CSP
+//
+//  Created by Singh, Kashish on 11/29/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public class InternetDetailViewController : UIViewController
+{
+    //Mark: GUI Controls
+    
+
+
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+        // Do any addital setup after laoding the view
+    }
+
+    private func setup() -> Void
+    {
+        
+    }
+}
