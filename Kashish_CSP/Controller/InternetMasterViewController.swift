982d20ce4c9d4802f4bb5cf80b978b414c7dfc5c
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-12-01 13:39:42 -0700

    Created a prepare for segue method which prepares for moving between screens.
    The preperation is things like changing text and getting ready to change to the new screen from the previous one.

--- Kashish_CSP/Controller/InternetMasterViewController.swift
+++ Kashish_CSP/Controller/InternetMasterViewController.swift
@@ -32,6 +32,13 @@ public class InternetMasterViewController : UITableViewController
         addresses = [
             "https://www.google.com",
         ]
+        
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as?
+                InternetDetailViewController
+        }
     }
     
     public override func viewDidLoad()
@@ -39,7 +46,7 @@ public class InternetMasterViewController : UITableViewController
         super.viewDidLoad()
         setup()
         //Uncomment the following line to preserve selection between presentations
-        self.clearSelectionOnViewWillAppear = false
+        self.clearsSelectionOnViewWillAppear = false
     }
     
     // MARK: - table view data source
@@ -47,6 +54,7 @@ public class InternetMasterViewController : UITableViewController
     override public func numberOfSections(in tableview: UITableView) -> Int
     {
         // #warning Incomplete implementation, return the number of sections
+        return 1
     }
     
     override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -61,5 +69,36 @@ public class InternetMasterViewController : UITableViewController
         cell.textLabel!.text = currentText
         
         return cell
+    }
+    
+    //MARK: Handle the internal transfer
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier! == "showDetail"
+        {
+            if let indexPath = self.tableView.indexPathForSelectedRow
+            {
+                let urlString = addresses[indexPath.row]
+                let pageText : String
+                
+                if indexPath.row == 0
+                {
+                    //TODO: Replace with your definitions - great time to use the """ operator
+                    pageText = "All the definitions you wrote........"
+                }
+                else
+                {
+                    pageText = internetTopics[indexPath.row]
+                }
+                
+                let controller = segue.destination as!
+                    InternetDetailViewController
+                
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
     }
 }
