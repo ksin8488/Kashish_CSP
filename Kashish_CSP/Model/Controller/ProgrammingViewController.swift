95456c4dbb73c16692eea02d2c47c206f5598936
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-12-13 12:20:42 -0700

    Added a new ViewController that is connected to the ProgrammingViewController with a button for a segue and added code to it
    Imported the SpriteKit into the ProgrammingViewController.

--- Kashish_CSP/Model/Controller/ProgrammingViewController.swift
+++ Kashish_CSP/Model/Controller/ProgrammingViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import SpriteKit
 
 public class ProgrammingViewController : UIViewController
 {
-    public override func viewDidLoad()
+    //MARK: UI Components
+    
+    override public func viewDidLoad() -> Void
     {
         super.viewDidLoad()
+        let gameScene = StartScene(size: view.bounds.size)
+        let gameView = view as! SKView
+        
+        gameView.showsFPS = true
+        gameView.showsNodeCount = true      //lets us know the amound of nodes
+        gameView.ignoresSiblingOrder = true
+        
+        gameScene.scaleMode = .resizeFill
+        gameView.presentScene(gameScene)
     }
 }
