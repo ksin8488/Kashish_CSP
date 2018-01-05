28667ae6b9111cd05596618e699e1f02af5d7740
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-01-05 13:44:58 -0700

    Added code to the BucketItemClass after linking all the labels into the class from the prototype cell.
    There are still some errors due to parts of the code not being added yet but so far 2 new methods have been added called randomEmoji() and updateCellView().

--- Kashish_CSP/Controller/DataView/BucketItemCell.swift
+++ Kashish_CSP/Controller/DataView/BucketItemCell.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class BucketItemCell: UITableViewCell {
 
+    @IBOutlet weak var bucketItemText: UILabel!
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    @IBOutlet weak var bucketItemSignature: UILabel!
+    
+    var bucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1f601
+        let emojiEnd = 0x1f64f
+        let symblStart = 0x1f680
+        let symbolEnd = 0x1f6c5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
+    private func updateCellView()
+    {
+        if (bucketItem != nil)
+        {
+            bucketItemSignature.text = bucketItem.itemAuthor
+            bucketItemText.text = bucketItem.itemContents
+        }
+        else
+        {
+           bucketItemSignature.text = "author goes here"
+            bucketItemText.text = "text goes here"
+        }
+        bucketItemSymbol.text = randomEmojiSymbol()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
