33ab89ac2a552a7c790854434205a59f3de57c27
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-01-09 11:40:38 -0700

    Coded the DataViewController class that loads a file as well as creating the TableView code.
    Self: This is the first time we are using file loading in Swift so take note of how it's done.
    NOTE: Currently there will be errors showing up due to BucketItem not being declared.

--- Kashish_CSP/Controller/DataView/DataViewController.swift
+++ Kashish_CSP/Controller/DataView/DataViewController.swift
@@ -10,6 +10,57 @@ import UIKit
 
 public class DataViewController : UITableViewController
 {
+    lazy var bucketList : [BucketItem] =
+    {
+            return loadBucketListFromFile()     //Loading a file
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0],author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
+        }
+        return items
+    }
+    
+    //MARK: TableView code
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection sectioin: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        //          currentCell.bucketItemsSignature.text = currentCell.bucketItem.itemAuthor
+        
+        return currentCell
+    }
+    
+    
     public override func viewDidLoad()
     {
         super.viewDidLoad()
